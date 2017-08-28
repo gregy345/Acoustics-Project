244c2263fbf2ddcc043408f5127ed9f09442c221
gregy345 <gy1863a@american.edu>
2017-08-28 16:02:06 -0400

    Add files via upload

--- SyntheticAperture.py
+++ SyntheticAperture.py
@@ -6,7 +6,6 @@ import sys
 from scipy.io.wavfile import read
 from scipy import fftpack, ndimage
 import math 
-import AperturePlot as ap
 import re
  
 """
@@ -26,7 +25,6 @@ def syntheticAperture(start, end, slow, fast, sound, SampleRateRedux):
 	samplerate = 44100/SampleRateRedux
 	# convert varibles from seconds to indices in file
 	iterations = int(float((end-start-fast)*(1/slow)))    
-	#iterations = int(((end-start-fast)*(1/slow)))  
 	StStart = int(float(start*samplerate))
 	StSlow = int(float(slow*samplerate))
 	StFast = int(float(fast*samplerate))
@@ -35,37 +33,11 @@ def syntheticAperture(start, end, slow, fast, sound, SampleRateRedux):
 	l = np.array(sound[1],dtype=float)
 	b = l[0::SampleRateRedux]
 	col3 = StFast
-	cc = np.zeros((iterations, col3), dtype=complex)
+	ttMat = np.zeros((iterations, col3), dtype=complex)
 	for j in range(0, iterations):
 		for i in range(0, col3):
-			cc[j,i] = b[i+(j*StSlow)] 
-	fftt = fftpack.fft2(cc) 
+			ttMat[j,i] = b[i+(j*StSlow)] 
+	fftt = fftpack.fft2(ttMat) 
 	fftlog = np.array(np.log10(fftt), dtype=complex) 
-	#ap.plotAperture(cc)
-	return cc, fftt, fftlog
-	#return cc, fftt, SampleRateRedux
-	#return cc, fftt
-"""	
-# test data
-#these are the varibles that will be input by user or program yet to be built
-start = 0  # in seconds    2.91
-end =  1   # in seconds   3.121   3.5 
-slow = 0.0003  # in seconds    0.001   
-fast = 0.2  # in seconds     0.05
-SampleRateRedux = 4
-# recorded wav file 
-sound = read("audioFile1.wav") 
+	return ttMat, fftt, fftlog
 
-start = 0  # in seconds    2.91
-end =  0.5   # in seconds   3.121   3.5 
-slow = 0.0003  # in seconds    0.001   
-fast = 0.2  # in seconds     0.05
-SampleRateRedux = 4
-# recorded wav file 
-sound = read("audioFile1.wav") 
-
-#syntheticAperture(start, end, slow, fast)
-test = syntheticAperture(start, end, slow, fast, sound, SampleRateRedux)
-print test
-
-"""
--- compareAperture.py
+++ compareAperture.py
@@ -9,8 +9,6 @@ import math
 import cmath
 from SyntheticAperture import syntheticAperture
 import pylab
-from AperturePlot import plotAperture
-#from confusionMatrix import conMat
 
 """
 Compute the synthetic appeture matrix
@@ -32,35 +30,11 @@ def compareAperture(start, end, slow, fast, SampleRateRedux, sound1, sound2):
 	row2 = fftt2.shape[0]
 	col2 = fftt2.shape[1]
 
-	#energyMat = np.zeros((row1,col1), dtype=float)
 	energyMat = np.zeros((row1,col1), dtype=complex)
 	for i in range(0, row1):	
 		for j in range(0, col1):
-			energyMat[i,j] = 20*(np.log10(np.linalg.norm(fftt1[i,j]))-np.log10(np.linalg.norm(fftt2[i,j])))
-	#return energyMat #, cc1
-	#emat = abs(energyMat)
-	#print fftt1
-	#print fftt2
-	print energyMat 
-	print "this is the average of the decibel level differences"
-	print np.average(energyMat)
-	#plotAperture(fftlog1)
-	#plotAperture(fftlog2)
-	#plotAperture(energyMat)
-	#conMat(cc1,cc2)
+			energyMat[i,j] = 20*np.abs((np.log10(np.linalg.norm(fftt1[i,j]))-np.log10(np.linalg.norm(fftt2[i,j]))))
+	energyAvg = np.average(energyMat)
+	return energyMat, energyAvg
 
-# test data
-#these are the varibles that will be input by user or program yet to be built
-start = 0  # in seconds    2.91
-end =  0.5   # in seconds   3.121   3.5 
-slow = 0.0003  # in seconds    0.001   
-fast = 0.2  # in seconds     0.05
-SampleRateRedux = 4
-
-# recorded wav file 
-sound1 = read("audioFile1.wav") 
-sound2 = read("audioFile2.wav") 
-
-#syntheticAperture(start, end, slow, fast)
-print compareAperture(start, end, slow, fast, SampleRateRedux, sound1, sound2)
 
